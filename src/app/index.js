3dce619d6ccaf9c31f7a527dcba5885c8c6c0f75
Rafal Kolasinski <rafal.kolasinski92@gmail.com>
2017-05-16 21:24:55 +0200

    components: added wrapping component

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,14 @@
-alert("Werks!")
+var React = require("react");
+var ReactDOM = require("react-dom");
+
+// Creating cmp
+var TodoComponent = React.createClass({
+    render: function () {
+        return(
+            <h1>REACT</h1>
+        );
+    }
+});
+
+// Putting cmp into HTML
+ReactDOM.render(<TodoComponent />, document.getElementById("todo-wrapper"));
