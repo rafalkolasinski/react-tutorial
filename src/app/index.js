9757ed85d567783e12176b1a17f6d70e1456d483
Rafal Kolasinski <rafal.kolasinski92@gmail.com>
2017-05-16 21:32:25 +0200

    props: added object & displayed it in the DOM

--- src/app/index.js
+++ src/app/index.js
@@ -5,10 +5,28 @@ var ReactDOM = require("react-dom");
 var TodoComponent = React.createClass({
     render: function () {
         return(
-            <h1>REACT</h1>
+            <div>
+                <h1>MOVIES LIST</h1>
+                <p>
+                    <strong>Movie name:&nbsp;</strong>{ this.props.movie.name }
+                </p>
+                <p>
+                    <strong>Movie rating:&nbsp;</strong>{ this.props.movie.rating }
+                </p>
+                <p>
+                    <strong>Movie income:&nbsp;</strong>{ this.props.movie.income }
+                </p>
+
+            </div>
         );
     }
 });
 
+var myMovie = {
+    name: "Movie 1",
+    rating: 9.75,
+    income: "10M"
+}
+
 // Putting cmp into HTML
-ReactDOM.render(<TodoComponent />, document.getElementById("todo-wrapper"));
+ReactDOM.render(<TodoComponent movie={ myMovie }/>, document.getElementById("todo-wrapper"));
