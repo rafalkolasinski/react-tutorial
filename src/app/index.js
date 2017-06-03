fa85bc0508f8d239efd6f44f19ffd7149c90c8f6
Rafal Kolasinski <rafal.kolasinski92@gmail.com>
2017-06-03 18:21:02 +0200

    handling events: added methods for removing list items based on basic events & event propagation in React

--- src/app/index.js
+++ src/app/index.js
@@ -12,20 +12,31 @@ var MoviesComponent = React.createClass({
     render: function () {
         var movies = this.state.movies.map(function(movie, index) {
            return(
-               <MovieItem item={ movie } key={ index }/>
+               <MovieItem item={movie} key={index} onDelete={this.onDelete}/>
            );
-        });
+        }.bind(this));
 
         return(
             <div id="movies-list">
                 <h1>The best QT movies in the world!</h1>
-                <p>{ this.state.rating }</p>
+                <p>{this.state.rating}</p>
                 <ul>
-                    { movies }
+                    {movies}
                 </ul>
             </div>
         );
-    } // render
+    }, // render
+
+    // Custom functions
+    onDelete: function(removedMovie) {
+        var updatedMovies = this.state.movies.filter(function(movie, index) {
+            return movie !== removedMovie;
+        });
+
+        this.setState({
+            movies: updatedMovies
+        });
+    }
 });
 
 // Creating MovieItem component
@@ -34,13 +45,19 @@ var MovieItem = React.createClass({
        return(
            <li>
                <div className="movie-item">
-                   <span className="movie-item-name">
-                       { this.props.item }
+                   <span className="item-name">
+                       {this.props.item}
                    </span>
+                   <span className="item-delete" onClick={this.handleDelete}>[X]</span>
                </div>
            </li>
        );
-   }
+   },
+
+    // Custom functions
+    handleDelete: function() {
+        this.props.onDelete(this.props.item);
+    }
 });
 
 // Putting cmp into HTML
